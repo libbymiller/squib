3dafaff455af2d322ce0e44c55916b4d427d8740
Andy Meneely <andy@se.rit.edu>
2015-04-13 21:38:36 -0400

    v0.5.0 docs

--- doc/samples/draw_shapes.rb
+++ doc/samples/draw_shapes.rb
@@ -15,5 +15,10 @@ Squib::Deck.new do
        x2: 150, y2: 650,
        stroke_width: 25.0
 
+  curve x1: 50,  y1: 850, cx1: 150, cy1: 700,
+        x2: 625, y2: 900, cx2: 150, cy2: 700,
+        stroke_width: 12.0, stroke_color: :cyan,
+        fill_color: :burgundy
+
   save_png prefix: 'shape_'
 end
